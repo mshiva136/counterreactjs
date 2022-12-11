6c8c05ba6fe0d533c070167221cf4ddd3dd5a94c
root <mshiva136@gmail.com>
2022-12-11 19:18:26 +0000

    counter

---
+++ src/components/Counter/index.js
@@ -0,0 +1,34 @@
+import {Component} from 'react'
+import './index.css'
+
+class Counter extends Component {
+  state = {count: 0}
+
+  onIncrement = () => {
+    this.setState(prevState => ({count: prevState.count + 1}))
+  }
+
+  onDecrement = () => {
+    this.setState(prevState => ({count: prevState.count - 1}))
+  }
+
+  render() {
+    const {count} = this.state
+    return (
+      <div className="container">
+        <h1 className="heading">Counter</h1>
+        <p className="count">{count}</p>
+        <div>
+          <button className="button" onClick={this.onIncrement}>
+            Increse
+          </button>
+          <button className="button" onClick={this.onDecrement}>
+            Decrese
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Counter
